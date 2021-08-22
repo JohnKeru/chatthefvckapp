d988d9b3d2969dc095a0ec698ab217740577bf7b
PC <keruk355@gmail.com>
2021-08-22 19:32:56 +0800

    chatupp

--- src/main/java/com/chatvp/WebSocketConfig.java
+++ src/main/java/com/chatvp/WebSocketConfig.java
@@ -13,10 +13,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins("http://localhost:3000")
+			.setAllowedOrigins("https://chatthefvckapp.web.app/")
 			.withSockJS();
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins("http://localhost:3000");
+			.setAllowedOrigins("https://chatthefvckapp.web.app/");
 	}
 
 	@Override
